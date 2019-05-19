8a3ecea0aff0f982feea358c9ffa93cbb08e56bd
Harshit Singh <31100101+harshitsingh5@users.noreply.github.com>
2019-05-19 18:30:44 +0530

    update

--- stack.java
+++ stack.java
@@ -4,17 +4,17 @@ class Stack
 	int top;
 	int a[] = new int[MAX];
 
-	public Stack()
+	public Stack()			//constructor
 	{
 		top = -1;
 	}
 
-	boolean isEmpty()
+	boolean isEmpty()		//check for empty
 	{
 		return (top < 0);
 	}
 
-	boolean push(int x)
+	boolean push(int x)		//push on top of stack
 	{
 		if (top >= (MAX-1))
 		{
@@ -29,7 +29,7 @@ class Stack
 		}
 	}
 
-	int pop()
+	int pop()			//pop from top of stack
 	{
 		if (top < 0)
 		{
@@ -49,9 +49,9 @@ class Main
 	public static void main(String args[])
 	{
 		Stack s = new Stack();
-		s.push(10);
-		s.push(20);
-		s.push(30);
-		System.out.println(s.pop() + " Popped from stack");
+		s.push(10);			//stack- 10
+		s.push(20);			//stack- 10,20
+		s.push(30);			//stack- 10,20,30
+		System.out.println(s.pop() + " Popped from stack");	//30 popped out
 	}
 }
